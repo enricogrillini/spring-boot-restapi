7e410b159830b8b3bb262e5b383517aae8ee1b50
Enrico Grillini <enrico.grillini@gmail.com>
2023-06-26 11:10:26 +0200

    pom

--- spring-boot-03-access-log/src/main/java/it/eg/cookbook/filter/AccessLogFilter.java
+++ spring-boot-03-access-log/src/main/java/it/eg/cookbook/filter/AccessLogFilter.java
@@ -4,11 +4,14 @@ import lombok.extern.slf4j.Slf4j;
 import org.slf4j.MDC;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
+import org.springframework.web.util.ContentCachingRequestWrapper;
+import org.springframework.web.util.WebUtils;
 
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 import java.util.UUID;
@@ -28,6 +31,7 @@ public class AccessLogFilter implements Filter {
         Instant start = Instant.now();
 
         String correlationId = req.getHeader(CORRELATION_ID_NAME);
+
         if (correlationId == null || "".equals(correlationId.trim())) {
             correlationId = generateUniqueCorrelationId();
         }
@@ -38,14 +42,19 @@ public class AccessLogFilter implements Filter {
 
             // Access Log IN
             if (doLog) {
-                log.info("IN  - method: {}, URI: {}, protocol {}, host: {}", req.getMethod(), req.getRequestURI(), req.getProtocol(), req.getRemoteHost());
+                log.info("IN  | {} {}{}", req.getMethod(), req.getRequestURI(), getQueryString(req));
+
+                String payload = this.getMessagePayload(req);
+                if (payload != null) {
+                    log.debug("    | payload {}", payload);
+                }
             }
 
             chain.doFilter(request, response);
 
             // Access Log OUT
             if (doLog) {
-                log.info("OUT - method: {}, URI: {}, protocol {}, host: {}, status {}, duration {}", req.getMethod(), req.getRequestURI(), req.getProtocol(), req.getRemoteHost(), resp.getStatus(), ChronoUnit.MILLIS.between(start, Instant.now()));
+                log.info("OUT | {} {}{} | status {}, duration {}", req.getMethod(), req.getRequestURI(), getQueryString(req), resp.getStatus(), ChronoUnit.MILLIS.between(start, Instant.now()));
             }
         }
     }
@@ -54,6 +63,33 @@ public class AccessLogFilter implements Filter {
         return new StringBuilder().append("generated:")
                 .append(UUID.randomUUID())
                 .toString();
+    }
+
+    private String getQueryString(HttpServletRequest request) {
+        String payload = request.getQueryString();
+        if (payload != null) {
+            return "?" + payload;
+        } else {
+            return "";
+        }
+    }
+
+    private String getMessagePayload(HttpServletRequest request) {
+        ContentCachingRequestWrapper wrapper = (ContentCachingRequestWrapper) WebUtils.getNativeRequest(request, ContentCachingRequestWrapper.class);
+        if (wrapper != null) {
+            byte[] buf = wrapper.getContentAsByteArray();
+            if (buf.length > 0) {
+                int length = Math.min(buf.length, 100);
+
+                try {
+                    return new String(buf, 0, length, wrapper.getCharacterEncoding()).replace("\n", "").replace("\r", "");
+                } catch (UnsupportedEncodingException var6) {
+                    return "[unknown]";
+                }
+            }
+        }
+
+        return null;
     }
 
 }
